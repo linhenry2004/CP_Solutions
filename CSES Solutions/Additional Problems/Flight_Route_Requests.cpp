770b3fcfba46cb9b6f693f1fb3b5626911019b92
Henry Lin <linhenry2004@gmail.com>
2022-07-16 22:53:26 +0800

    CSES Additional Problems

--- CSES Solutions/Additional Problems/Flight_Route_Requests.cpp
+++ CSES Solutions/Additional Problems/Flight_Route_Requests.cpp
@@ -13,5 +13,55 @@ void DFS (int u) {
     }
     vis[u] = 1;
     V.push_back(u);
-    
+    for (auto [v, d] : G[u]) {
+        if (d) {
+            in[v]++;
+        }
+        DFS(v);
+    }
+}
+
+int topu (int u) {
+    if (vis[u]) {
+        return 0;
+    }
+    int nc = 0;
+    V.clear();
+    DFS(u);
+    queue<int> Q;
+    for (int v : V) {
+        if (!in[v]) {
+            Q.push(v);
+        }
+    }
+    while (!Q.empty()) {
+        u = Q.front();
+        Q.pop();
+        nc++;
+        for (auto [v, d] : G[u]) {
+            if (!d) {
+                continue;
+            }
+            in[v]--;
+            if (!in[v]) {
+                Q.push(v);
+            }
+        }
+    }
+    return nc == V.size() ? V.size() - 1 : V.size();
+}
+
+int main () {
+    int n, m, a, b, edge = 0;
+    cin >> n >> m;
+    while (m--) {
+        cin >> a >> b;
+        G[a].push_back({b, 1});
+        G[b].push_back({a, 0});
+    }
+    for (int i = 1; i <= n; i++) {
+        edge += topu(i);
+    }
+    cout << edge << endl;
+    return 0;
 }
