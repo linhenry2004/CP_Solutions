033717744534a69e017656ef0e5c26aa1c9cf9da
Henry Lin <linhenry2004@gmail.com>
2022-07-22 11:48:04 +0800

    Leetcode

---
+++ LeetCode/Rotate_Array.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> t = nums;
+        for (int i = 0; i < nums.size(); i++) {
+            nums[(i + k) % nums.size()] = t[i];
+        }
+    }
+};
